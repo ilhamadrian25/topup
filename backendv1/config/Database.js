f9dba79183324ccdcab56672fb5836c062ef4622
Mochammad Ilham Adrian <95473896+ilhamadrian25@users.noreply.github.com>
2023-11-08 16:43:38 +0700

    Update Database.js

--- backendv1/config/Database.js
+++ backendv1/config/Database.js
@@ -2,11 +2,11 @@ import Sequelize from 'sequelize'
 
 const db = new Sequelize({
     dialect: 'mysql',
-    host: 'localhost',
+    host: 'db4free.net',
     port: 3306,
-    username: 'id20314206_ilhamadrian',
-    password: '@MuhamadIlham77',
-    database: 'id20314206_ilhamadrian'
+    username: 'epiz_32618037_te',
+    password: 'epiz_32618037_te',
+    database: 'epiz_32618037_te'
 });
 
 export default db;
