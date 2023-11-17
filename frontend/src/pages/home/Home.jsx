4f5a05e7e3e814addc2609c34306a9f99ba1ce94
ilham <muhamadilhamadrian0@gmail.com>
2023-11-18 03:04:16 +0700

    sd

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     <>
       <div className='py-14 hp:px-2 md:px-6 lg:px-36 bg-primary'>
         <div className="hp-2:mx-3 sm:mx-14 md:mx-16 lg:mx-20 3xl:mx-24">
-          <Carousel />
+          {/* <Carousel /> */}
           {/* <Tab /> */}
         </div>
       </div>
